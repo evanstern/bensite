7479f058c75a557ee195dc80a0005d8333859aae
Evan Stern <suriansg@gmail.com>
2017-09-16 16:55:59 -0400

    Added stuff to lessons 3 and 4

--- 04-lesson/script.js
+++ 04-lesson/script.js
@@ -4,3 +4,13 @@ setTimeout(function () {
     element.style.display = 'block';
 }, 3000);
 
+$(document).ready(function () {
+    var $addButton = $('#Add-Item');
+
+    $addButton.click(function () {
+        var $myList = $('ul.fn-list');
+        var $newItem = $('<li>Wolfgang</li>');
+        $myList.append($newItem);
+    });
+});
+
